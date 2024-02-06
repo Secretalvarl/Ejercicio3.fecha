194e1dfcca69483e3556cf2dcfba99683355253a
DESKTOP-R8KP20E\Profesor <csi23-agarera@colegioaltair.net>
2024-02-06 13:06:45 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio3.fecha/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Ejercicio3.fecha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime fechaActual = DateTime.Now;
+            Random random = new Random();
+
+            Console.WriteLine(fechaActual.ToString("dd-MM-yyyy"));
+
+            DateTime fechaDia = fechaActual.AddDays(1);
+
+            DateTime fechaMes = fechaDia.AddMonths(-24);
+
+            DateTime fechaAño = fechaMes.AddYears(random.Next(0, 4));
+
+            Console.WriteLine(fechaAño.ToString("dd-MM-yyyy"));
+
+            if(fechaActual > fechaAño) 
+            {
+                Console.WriteLine("El hoy es más grande");
+            }
+            else
+            {
+                Console.WriteLine("El mañana es mas pequeño");
+            }
+            
+
+           
+        }
+    }
+}
